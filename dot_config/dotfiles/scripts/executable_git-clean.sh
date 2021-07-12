1ba49cfae6c9f568a5bf1fab1642a627f7b3ec53
Joseph Diza <josephm.diza@gmail.com>
2021-07-11 20:00:49 -0700

    Add git gc clean script

---
+++ dot_config/dotfiles/scripts/executable_git-clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+show_usage() {
+    echo '
+Usage: git-clean.sh [folder]
+'
+}
+
+folder=$1
+if [[ -z $folder ]]; then
+    show_usage;
+else
+    find /$folder -name '*.git' -execdir sh -c 'cd {} && git gc' \;
+fi

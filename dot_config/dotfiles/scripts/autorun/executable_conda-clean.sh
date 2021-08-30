a52ed53c5e9e0358f029a0789f4d616d3c5286d9
Joseph Diza <josephm.diza@gmail.com>
2021-08-29 22:28:49 -0700

    Update and fix conda-clean.sh

--- dot_config/dotfiles/scripts/autorun/executable_conda-clean.sh
+++ dot_config/dotfiles/scripts/autorun/executable_conda-clean.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-conda clean -a
+# conda-clean removes all pkgs that are unused in all conda environments
+shopt -s expand_aliases
+conda-init
+sudo conda clean -a -y
+notify-send "Conda" "All unused packages and caches in all conda environments have been removed."

275a0da16a6c21f7d82b408e4827a874fd371542
Joseph Diza <josephm.diza@gmail.com>
2021-09-16 22:36:58 -0700

    Fix sdkman.sh

--- dot_config/dotfiles/scripts/terminal/executable_sdkman.sh
+++ dot_config/dotfiles/scripts/terminal/executable_sdkman.sh
@@ -1,5 +1,6 @@
 #!/bin/bash/
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+XDG_CONFIG_HOME=/home/jmd/.config
 export SDKMAN_DIR="$XDG_CONFIG_HOME/sdkman"
-[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+[[ -e "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"

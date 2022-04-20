e5139f1ee30b85c49cdbd457853a8a6202a46ee5
Joseph Diza <josephm.diza@gmail.com>
2022-04-20 00:12:10 -0700

    Add quickinstall, gitoxide rust crates

--- dot_config/dotfiles/scripts/init/executable_rust.sh
+++ dot_config/dotfiles/scripts/init/executable_rust.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cargo install cargo-cache sccache slint-lsp slint-viewer hvm cargo-deb cargo-fuzz cargo-generate wasm-pack
+cargo install cargo-cache sccache slint-lsp slint-viewer hvm cargo-deb cargo-fuzz cargo-generate wasm-pack cargo-quickinstall gitoxide

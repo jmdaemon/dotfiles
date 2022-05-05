d664b0b6f483a86e229abc1d0743e9dd8e2ee3d4
Joseph Diza <josephm.diza@gmail.com>
2022-05-05 12:01:26 -0700

    Fix sshkeygen.sh script

--- dot_config/dotfiles/scripts/init/executable_sshkeygen.sh
+++ dot_config/dotfiles/scripts/init/executable_sshkeygen.sh
@@ -20,15 +20,29 @@ fi
 # Select key type
 key=""
 if [[ "$keytype" == "RSA" ]]; then
-    key="-t rsa -b 2048"
+    #key="-t rsa -b 2048"
+    key="-t rsa -b 4096"
 elif [[ "$keytype" == "ED25519" ]]; then
     key="-t ed25519"
+else
+    echo "You must provide a keytype."
+    echo "Keytypes available are: RSA, ED25519"
+    exit 2
 fi
 
-# Generate key
-ssh-keygen "$key" -C "$email" -f "$SSH_DIR/$keyname"
-eval "$(ssh-agent -s)"
-ssh-add "$SSH_DIR/$keyname"
+keyfile="$sshfp/$keyname"
 
-# Copy to clipboard
-[[ $(command -v xclip) ]] && cat "$SSH_DIR/$keyname.pub" | xclip -sel clip || :
+# Generate key
+ssh-keygen $key -C "$email" -f $keyfile
+
+# Set correct permissions on keyfile
+chmod 600 $keyfile
+
+if [[ -f $keyfile ]]; then
+    # Add the ssh key
+    eval "$(ssh-agent -s)"
+    ssh-add "$keyfile"
+
+    # Copy the public key to the clipboard
+    [[ $(command -v xclip) ]] && cat "$sshfp/$keyname.pub" | xclip -sel clip || :
+fi

4842e06633cc7583f305487cfcdb6210e64b283b
JMD <josephm.diza@gmail.com>
2022-08-10 13:32:45 -0700

    Remove dead code in mkgesture.ps1

--- Powershell Scripts/mkgesture.ps1
+++ Powershell Scripts/mkgesture.ps1
@@ -1,16 +1,11 @@
+# mcode.ps1 - Encodes any audio file to .wav with increases volume
+
 param(
     [String]$volume="None",
     [Parameter(Mandatory=$true)][String]$i
 
 )
-# mcode.ps1 - Encodes any audio file to .wav with increases volume
 
-
-# This will get the basename of a file that exists on disk
-# Note that you must pass in the fully qualified path
-# $base = (Get-Item $i).Basename
-
-# This does not require a fully qualified path
 $base = [io.path]::GetFileNameWithoutExtension($i)
 Write-Output "Re-encoding $base"
 

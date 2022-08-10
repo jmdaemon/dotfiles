98ce013b17bc281d03609f23a8299d2d2ae69f86
JMD <josephm.diza@gmail.com>
2022-08-10 13:36:00 -0700

    Remove unneeded towav scripts

--- Powershell Scripts/towav.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    [Parameter()]
-    [String]$i
-)
-$base = [io.path]::GetFileNameWithoutExtension($i)
-Write-Output $base
-ffmpeg -i "$i"-r 44100 "${base}.wav"
--- Powershell Scripts/towav2.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    [Parameter()]
-    [String]$i
-)
-$base = [io.path]::GetFileNameWithoutExtension($i)
-Write-Output $base
-ffmpeg -i "$i"-ar 44100 -filter:a "volume=1.5" "${base}.wav"

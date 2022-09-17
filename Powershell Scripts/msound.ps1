8441e2f979ca1f0601553158cbb7a732312b01d0
JMD <josephm.diza@gmail.com>
2022-09-17 01:07:58 -0700

    Change parameters of msound.ps1 to be more "unix-like"

--- Powershell Scripts/msound.ps1
+++ Powershell Scripts/msound.ps1
@@ -1,17 +1,17 @@
 # msound.ps1 - Rencodes any audio file to .wav with increases volume using Ffmpeg
-# Usage: msound.ps1 -in "input.mp3" -out "output.wav" 
+# Usage: msound.ps1 -i "input.mp3" -o "output.wav" 
 
 param(
     [String]$volume="None",
-    [Parameter(Mandatory=$true)][String]$in,
-    [Parameter(Mandatory=$true)][String]$out
+    [Parameter(Mandatory=$true)][String]$i,
+    [Parameter(Mandatory=$true)][String]$o
 )
 
 # Re-encode audio to WAV format, with audio bitrate of 44.1Hz and optional volume increase
 if ($volume -eq "None") {
-    Write-Output "ffmpeg -ar 44100 -i \"$in\" \"$out\""
-    ffmpeg -i "$in" -ar 44100  "$out"
+    Write-Output "ffmpeg -ar 44100 -i \"$i\" \"$o\""
+    ffmpeg -i "$i" -ar 44100  "$o"
 } else {
-    Write-Output "ffmpeg -ar 44100 -i "$in" -filter:a \"volume=${volume}\" \"$out\""
-    ffmpeg -i "$in" -ar 44100 -filter:a "volume=${volume}" "$out"
+    Write-Output "ffmpeg -ar 44100 -i "$i" -filter:a \"volume=${volume}\" \"$o\""
+    ffmpeg -i "$i" -ar 44100 -filter:a "volume=${volume}" "$o"
 }

b72641aa0e5b024b6387fbc3f65e6fc2a84f3df7
Joseph Diza <josephm.diza@gmail.com>
2023-06-17 16:57:29 -0700

    Create $src if it doesn't exist
    Fix all bugs with script

--- dot_local/scripts/powershell/symlink_all.ps1
+++ dot_local/scripts/powershell/symlink_all.ps1
@@ -1,3 +1,4 @@
+# Creates all symlinks defined in $links
 
 # Note: Super user/Admin privileges are required
 $links = @{
@@ -6,7 +7,7 @@ $links = @{
 }
 
 # Create a symlink
-function ln ([string] $src, [string] $dest) {
+function ln([string] $src, [string] $dest) {
     New-Item -Path $dest -ItemType SymbolicLink -Value $src
 }
 
@@ -27,42 +28,28 @@ function mkdirs([string] $path) {
 
 # Creates the symlink if $src exists and $dest does not
 function create_symlink([string] $src, $dest) {
-    if ((exists $src) -and (-not (exists $dest))) {
+    # Makes $src directory if it does not already exist
+    if (-not(exists $src)) {
+        mkdirs $src
+    }
+
+    if (-not (exists $dest)) {
         # Create all parent directories if they don't exist
         $parent = get_parent_dir $dest
         if (-not(exists $parent)) {
             mkdirs $parent
         }
-        # Create symlink
+        # Symlink directories
         echo "Symlinking $src -> $dest"
         ln $src $dest
     } elseif (exists $dest) {
-        echo "$desc already exists"
+        echo "$dest already exists"
     }
-}
-
-# Log Paths
-function log_paths([string] $src, [string] $dest) {
-    echo "Source: ${src}"
-    echo "Dest: ${dest}"
-    echo ""
 }
 
 # Symlink all our desired links
 foreach ($link in $links.GetEnumerator()) {
-    
-    #$src = $link.Item1
-    #$dest = $link.Item2
-    #($src, $dest) = ($link.Item1, $link.Item2)
-    # ($src, $dest) = ($link[0], $link[1])
-    #$src = $link.Get(0)
-    #$dest = $link.Get(1)
-
-    #Write-Host "$($link.Name) : $($link.Value)"
-
     $src = $link.Name
     $dest = $link.Value
-    #log_paths $link.Name $link.Value
-
     create_symlink $src $dest
 }
